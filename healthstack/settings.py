6e9e9c46c63b7795e10098e8dd36e92fb500a566
TimzonaTimothy <timzonatimothy@gmail.com>
2023-12-02 07:20:34 +0100

    vercel deployment

--- healthstack/settings.py
+++ healthstack/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 INTERNAL_IPS = [
